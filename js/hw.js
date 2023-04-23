96611df757f5f196c54b2793b30ceea763de4d46
amosovandrey <rivercity.dp@gmail.com>
2023-04-23 13:28:03 +0300

    Update hw.js

--- js/hw.js
+++ js/hw.js
@@ -1,73 +1,81 @@
 const users = [
   {
-    name: "Moore Hensley",
-    email: "moorehensley@indexia.com",
-    eyeColor: "blue",
-    friends: ["Sharron Pace"],
+    name: 'Moore Hensley',
+    email: 'moorehensley@indexia.com',
+    eyeColor: 'blue',
+    friends: ['Sharron Pace'],
     isActive: false,
     balance: 2811,
-    gender: "male"
+    gender: 'male',
   },
   {
-    name: "Sharlene Bush",
-    email: "sharlenebush@tubesys.com",
-    eyeColor: "blue",
-    friends: ["Briana Decker", "Sharron Pace"],
+    name: 'Sharlene Bush',
+    email: 'sharlenebush@tubesys.com',
+    eyeColor: 'blue',
+    friends: ['Briana Decker', 'Sharron Pace'],
     isActive: true,
     balance: 3821,
-    gender: "female"
+    gender: 'female',
   },
   {
-    name: "Ross Vazquez",
-    email: "rossvazquez@xinware.com",
-    eyeColor: "green",
-    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+    name: 'Ross Vazquez',
+    email: 'rossvazquez@xinware.com',
+    eyeColor: 'green',
+    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
     isActive: false,
     balance: 3793,
-    gender: "male"
+    gender: 'male',
   },
   {
-    name: "Elma Head",
-    email: "elmahead@omatom.com",
-    eyeColor: "green",
-    friends: ["Goldie Gentry", "Aisha Tran"],
+    name: 'Elma Head',
+    email: 'elmahead@omatom.com',
+    eyeColor: 'green',
+    friends: ['Goldie Gentry', 'Aisha Tran'],
     isActive: true,
     balance: 2278,
-    gender: "female"
+    gender: 'female',
   },
   {
-    name: "Carey Barr",
-    email: "careybarr@nurali.com",
-    eyeColor: "blue",
-    friends: ["Jordan Sampson", "Eddie Strong"],
+    name: 'Carey Barr',
+    email: 'careybarr@nurali.com',
+    eyeColor: 'blue',
+    friends: ['Jordan Sampson', 'Eddie Strong', 'Adrian Cross'],
     isActive: true,
     balance: 3951,
-    gender: "male"
+    gender: 'male',
   },
   {
-    name: "Blackburn Dotson",
-    email: "blackburndotson@furnigeer.com",
-    eyeColor: "brown",
-    friends: ["Jacklyn Lucas", "Linda Chapman"],
+    name: 'Blackburn Dotson',
+    email: 'blackburndotson@furnigeer.com',
+    eyeColor: 'brown',
+    friends: ['Jacklyn Lucas', 'Linda Chapman', 'Adrian Cross', 'Solomon Fokes'],
     isActive: false,
     balance: 1498,
-    gender: "male"
+    gender: 'male',
   },
   {
-    name: "Sheree Anthony",
-    email: "shereeanthony@kog.com",
-    eyeColor: "brown",
-    friends: ["Goldie Gentry", "Briana Decker"],
+    name: 'Sheree Anthony',
+    email: 'shereeanthony@kog.com',
+    eyeColor: 'brown',
+    friends: ['Goldie Gentry', 'Briana Decker'],
     isActive: true,
     balance: 2764,
-    gender: "female"
-  }
-]
+    gender: 'female',
+  },
+];
 
+// Дополни функцию getTotalBalanceByGender(users, gender) так, 
+// чтобы она возвращала общий баланс пользователей(свойство balance),
+//   пол которых(свойство gender) совпадает со значением параметра gender.
 
-const getTotalFriendCount = users => {
-return users.reduce((total, user) => { total },  [])   
+// Change code below this line
+const getTotalBalanceByGender = (users, gender) => {
+  return users.filter(user => user.gender === gender).reduce((total, user) => total + user.balance, 0);
 };
+// Change code above this line
 
+console.table('getTotalBalanceByGender', getTotalBalanceByGender(users, "male"));
 
-console.table('getTotalFriendCount', getTotalFriendCount(users));
+// console.log('sortedByReversedAuthorName', sortedByReversedAuthorName);
+// console.log('sortedByAscendingRating', sortedByAscendingRating);
+// console.log('sortedByDescentingRating', sortedByDescentingRating);
